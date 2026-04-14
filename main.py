d41e185f3223da9aacdb615a7420b5fc8e2887a8
juanigremes <juanigremes@gmail.com>
2026-04-14 14:42:34 -0300

    inicio del proyecto

---
+++ main.py
@@ -0,0 +1,15 @@
+# Imports
+
+import pygame 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+
+# Main
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
